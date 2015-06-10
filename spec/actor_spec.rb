cad81dac687924736cc3ef8c3cd7d8bd54219f6f
audrie & steven francisco <student@epicodus.com>
2015-06-10 13:50:45 -0700

    Passes spec for initialize method for actor class.

--- spec/actor_spec.rb
+++ spec/actor_spec.rb
@@ -1,1 +1,10 @@
 require('spec_helper')
+
+describe(Actor) do
+  describe('#initialize') do
+    it('is initialized with a name') do
+      test_actor = Actor.new({:name => 'Brad Pitt', :id => nil})
+      expect(test_actor).to(be_an_instance_of(Actor))
+    end
+  end
+end
