9bcca0847c95015b3a520dfaf4d33070d2cfa470
audrie & steven francisco <student@epicodus.com>
2015-06-10 13:53:26 -0700

    Passes spec for second half of initialize method for actor class.

--- spec/actor_spec.rb
+++ spec/actor_spec.rb
@@ -6,5 +6,9 @@ describe(Actor) do
       test_actor = Actor.new({:name => 'Brad Pitt', :id => nil})
       expect(test_actor).to(be_an_instance_of(Actor))
     end
+    it('can be initialized with its database ID') do
+      test_actor = Actor.new({:name => 'Brad Pitt', :id => 1})
+      expect(test_actor).to(be_an_instance_of(Actor))
+    end
   end
 end
